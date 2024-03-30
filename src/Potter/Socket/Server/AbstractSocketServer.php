6a1bd8b63f68cbd2e3d3722fe3704ea058d36a9e
Jay Potter <j@ypotter.ca>
2024-03-30 13:31:10 -0400

    Create AbstractSocketServer.php

---
+++ src/Potter/Socket/Server/AbstractSocketServer.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Socket\Server;
+
+abstract class AbstractSocketServer implements SocketServerInterface
+{
+    abstract public function listen(): void;
+}
